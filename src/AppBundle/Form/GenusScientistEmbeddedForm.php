a0005a31783bf47100c0cd773d2e15cf066bf46c
sruj <chryplewiczpawel@wp.pl>
2017-12-31 13:47:52 +0100

    Form Events - disabled dla pól z db

--- src/AppBundle/Form/GenusScientistEmbeddedForm.php
+++ src/AppBundle/Form/GenusScientistEmbeddedForm.php
@@ -8,6 +8,8 @@ use AppBundle\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GenusScientistEmbeddedForm extends AbstractType
@@ -15,15 +17,37 @@ class GenusScientistEmbeddedForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-                ->add('user', EntityType::class, [
+            ->addEventListener(
+                FormEvents::POST_SET_DATA,
+                array($this, 'onPostSetData')
+            )
+            ->add('yearsStudied');
+    }
+
+    public function onPostSetData(FormEvent $event)
+    {
+        if ($event->getData() && $event->getData()->getId()) {
+            $form = $event->getForm();
+            $form->add('user', EntityType::class, [
+                'placeholder' => 'Choose',
+                'class' => User::class,
+                'disabled' => true,
+                'choice_label' => 'email',
+                'query_builder' => function (UserRepository $repo) {
+                    return $repo->createIsScientistQueryBuilder();
+                }
+            ]);
+        } else {
+            $form = $event->getForm();
+            $form->add('user', EntityType::class, [
                 'placeholder' => 'Choose',
                 'class' => User::class,
                 'choice_label' => 'email',
-                'query_builder' => function(UserRepository $repo){
+                'query_builder' => function (UserRepository $repo) {
                     return $repo->createIsScientistQueryBuilder();
                 }
-            ])
-            ->add('yearsStudied');
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
